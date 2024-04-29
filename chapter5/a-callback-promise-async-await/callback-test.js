3df1ab07c6c88e645000af3f3734965332f512fa
soohyun <wjdtngus9536@gmail.com>
2024-04-29 22:57:11 +0900

    update

---
+++ chapter5/a-callback-promise-async-await/callback-test.js
@@ -0,0 +1,28 @@
+const DB = [];
+
+// 회원 가입 API 함수
+function register(user){ // 3중 콜백 함수
+    return saveDB(user, function (user){
+        return sendEmail(user, function (user) {
+            return getResult(user);
+        });
+    });
+}
+
+function saveDB(user, callback) {
+    DB.push(user);
+    console.log(`save ${user.name} to DB`);
+    return callback(user);
+}
+
+function sendEmail(user, callback) {
+    console.log(`email to ${user.email}`);
+    return callback(user);
+}
+
+function getResult(user){
+    return `success register %{user.name}`;
+}
+
+const result = register({ email: "andy@test.com", password: "1234", name: "andy"});
+console.log(result);
