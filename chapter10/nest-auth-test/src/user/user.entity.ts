40fcb8e4837556bf37f29a264460a6feabc05a18
soohyun <wjdtngus9536@gmail.com>
2024-07-07 23:53:38 +0900

    add annotation

--- chapter10/nest-auth-test/src/user/user.entity.ts
+++ chapter10/nest-auth-test/src/user/user.entity.ts
@@ -1,11 +1,12 @@
+// 1) 데코레이터 임포트
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
-@Entity()
+@Entity()           // 2) 엔티티 객체임을 알려주기 위한 데코레이터
 export class User {
     @PrimaryGeneratedColumn()
-    id?: number;
+    id?: number;    // 3) id는 pk이며 자동 증가하는 값
 
-    @Column({ unique: true})
+    @Column({ unique: true}) // 4) email은 유니크한 값
     email: string;
 
     @Column()
@@ -15,5 +16,5 @@ export class User {
     username: string;
 
     @Column({ type: "datetime", default: () => "CURRENT_TIMESTAMP" })
-    createdDt: Date = new Date();
+    createdDt: Date = new Date(); // 5) 기본값을 넣어줌
 }
--- chapter10/nest-auth-test/src/user/user.service.ts
+++ chapter10/nest-auth-test/src/user/user.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm'; // Repositoey 의존성 주입
+import { InjectRepository } from '@nestjs/typeorm'; // 1) Repositoey 의존성 주입, 데코레이터
 import { User } from './user.entity';
-import { Repository } from 'typeorm'; // typeorm의 Repository 저장, 읽기 같은 기본적인 메서드들을 제공
+import { Repository } from 'typeorm'; // 2) typeorm의 Repository : 저장, 읽기 같은 기본적인 메서드들을 제공
 
 @Injectable()
 export class UserService {
     constructor(
-        @InjectRepository(User) 
-        private userRepository: Repository<User>
+        // 3) 리포지토리 주입
+        @InjectRepository(User) // User 타입의 리포지토리를 주입한다고 알려줌
+        private userRepository: Repository<User> // 변수 선언, 타입은 Repository<User>
     ) {}
 }
