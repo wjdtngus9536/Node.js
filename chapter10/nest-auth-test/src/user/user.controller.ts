74c81dc179e52b5e13171c73fd3e0ed9adfd1456
soohyun <wjdtngus9536@gmail.com>
2024-07-08 23:37:17 +0900

    add user.controller.ts

--- chapter10/nest-auth-test/src/user/user.controller.ts
+++ chapter10/nest-auth-test/src/user/user.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, Param, Put, Delete } from '@nestjs/common';
+import { User } from './user.entity';
+import { UserService } from './user.service';
 
-@Controller('user')
-export class UserController {}
+@Controller('user') // 1) 컨트롤러 설정 데코레이터
+export class UserController {
+    constructor(private userService: UserService) {} // 2) 유저 서비스 주입
+
+    @Post('/create')
+    createUser(@Body() user: User) { // 3) 유저 생성
+        return this.userService.createUser(user);
+    }
+
+    @Get('/getUser/:email')
+    async getUser(@Param('email') email: string) { // 4) 한 명의 유저 찾기
+        const user = await this.userService.getUser(email);
+        console.log(user);
+        return user;
+    }
+
+    @Put('/update/:email')
+    // 5) 유저 정보 업데이트
+    updateUser(@Param('email') email: string, @Body() user: User) {
+        console.log(user);
+        return this.userService.updateUser(email, user);
+    }
+
+    @Delete('/delete/:email')
+    deleteUser(@Param('email') email: string) { // 6) 유저 삭제
+        return this.userService.deleteUser(email);
+    }
+}
