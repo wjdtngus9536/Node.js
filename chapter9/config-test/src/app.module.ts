dbacbf584ff77686f71972fe602a61fc6178d579
soohyun <wjdtngus9536@gmail.com>
2024-09-30 23:24:39 +0900

    update

--- chapter9/config-test/src/app.module.ts
+++ chapter9/config-test/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+// ConfigModule은 보통 app.module.ts에서 해당 코드 실행
+import { ConfigModule } from '@nestjs/config';
+import { WeatherModule } from './weather/weather.module';
+
+console.log('env : ' + process.env.NODE_ENV);
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true
+  }), WeatherModule],
   controllers: [AppController],
   providers: [AppService],
 })
